4c1727aa0d8921a83c131ddbd6c15ffb28a1e5b3
Typeset <76000651+typeset-design@users.noreply.github.com>
2021-02-08 10:55:36 +0000

    Update SectionContainer.js

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,3 +1,6 @@
 export default function SectionContainer({ children }) {
-  return <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+  return 
+          <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-5 lg:col-gap-10 row-gap-10 md:row-gap-10 mb-32">
+            <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
+   {children}</div></div>
 }
